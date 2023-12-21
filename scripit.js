73d798e0a49d62bbb7cb455b77a2fc3aff99fe54
Mehdi Fanou <152438078+MehdiFanou@users.noreply.github.com>
2023-12-21 13:01:19 -0500

    Website of my own making
    Add functionalities such as login/registration menu where individuals can make there own accounts and login with the website with it. The primary languages I used are HTML, CSS, Java-scripit.

---
+++ scripit.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerlink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
